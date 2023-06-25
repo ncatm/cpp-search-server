48b28d2bdb88bb1c6195bccfaca110394904dbaf
Daniel <daniel.nch@yandex.ru>
2023-06-25 14:52:48 +0300

    3.2

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -124,8 +124,33 @@ void TestCalculateDocumentRating() {
         ASSERT_EQUAL(found_docs[0].rating, average);
     }
 }
+
+void TestCalculateDocumentMinusRating() {
+    SearchServer server;
+    const vector<int> ratings = { -9, -10, -4 };
+    const int average = (-9 -10 -4) / 3;
+    server.AddDocument(0, "fluffy cat fluffy tail"s, DocumentStatus::ACTUAL, ratings);
  
+    {
+        const auto found_docs = server.FindTopDocuments("fluffy cat"s);
+        ASSERT_EQUAL(found_docs.size(), 1u);
+        ASSERT_EQUAL(found_docs[0].rating, average);
+    }
+}
+
+void TestCalculateDocumentMixedRating() {
+    SearchServer server;
+    const vector<int> ratings = { 9, 10, -4 };
+    const int average = (9 + 10 -4) / 3;
+    server.AddDocument(0, "fluffy cat fluffy tail"s, DocumentStatus::ACTUAL, ratings);
  
+    {
+        const auto found_docs = server.FindTopDocuments("fluffy cat"s);
+        ASSERT_EQUAL(found_docs.size(), 1u);
+        ASSERT_EQUAL(found_docs[0].rating, average);
+    }
+}
+
 void TestDocumentSearchByPredicate() {
     SearchServer server;
     server.AddDocument(100, "cat in the city"s, DocumentStatus::ACTUAL, { 1, 2, 3 });
@@ -137,9 +162,29 @@ void TestDocumentSearchByPredicate() {
         ASSERT_HINT(found_docs[0].id == 100, "Wrong predicate");
     }
 }
- 
 
-void TestDocumentSearchByStatus() {
+void TestDocumentSearchByStatusActual() {
+    const int doc_id1 = 42;
+    const int doc_id2 = 43;
+    const int doc_id3 = 44;
+    const string content1 = "cat in the city"s;
+    const string content2 = "cat in the town"s;
+    const string content3 = "cat in the town or city"s;
+    const vector<int> ratings = { 1, 2, 3 };
+    SearchServer server;
+    server.AddDocument(doc_id1, content1, DocumentStatus::IRRELEVANT, ratings);
+    server.AddDocument(doc_id2, content2, DocumentStatus::ACTUAL, ratings);
+    server.AddDocument(doc_id3, content3, DocumentStatus::ACTUAL, ratings);
+    const auto found_docs = server.FindTopDocuments("in the cat"s, DocumentStatus::ACTUAL);
+ 
+    {
+        ASSERT_HINT(found_docs[0].id == doc_id2, "Wrong status");
+        ASSERT_HINT(found_docs[1].id == doc_id3, "Wrong status");
+        ASSERT_HINT(found_docs.size() == 2, "Wrong status request");
+    }
+}
+
+void TestDocumentSearchByStatusIrrelevant() {
     const int doc_id1 = 42;
     const int doc_id2 = 43;
     const int doc_id3 = 44;
@@ -159,15 +204,85 @@ void TestDocumentSearchByStatus() {
         ASSERT_HINT(found_docs.size() == 2, "Wrong status request");
     }
 }
- 
 
-void TestCalculateRelevance() {
+
+
+void TestDocumentSearchByStatusBanned() {
+    const int doc_id1 = 42;
+    const int doc_id2 = 43;
+    const int doc_id3 = 44;
+    const string content1 = "cat in the city"s;
+    const string content2 = "cat in the town"s;
+    const string content3 = "cat in the town or city"s;
+    const vector<int> ratings = { 1, 2, 3 };
     SearchServer server;
-    server.AddDocument(100, "white cat with new ring"s, DocumentStatus::ACTUAL, { 1, 2, 3 });
+    server.AddDocument(doc_id1, content1, DocumentStatus::ACTUAL, ratings);
+    server.AddDocument(doc_id2, content2, DocumentStatus::BANNED, ratings);
+    server.AddDocument(doc_id3, content3, DocumentStatus::BANNED, ratings);
+    const auto found_docs = server.FindTopDocuments("in the cat"s, DocumentStatus::BANNED);
+ 
+    {
+        ASSERT_HINT(found_docs[0].id == doc_id2, "Wrong status");
+        ASSERT_HINT(found_docs[1].id == doc_id3, "Wrong status");
+        ASSERT_HINT(found_docs.size() == 2, "Wrong status request");
+    }
+}
+
+void TestDocumentSearchByStatusRemoved() {
+    const int doc_id1 = 42;
+    const int doc_id2 = 43;
+    const int doc_id3 = 44;
+    const string content1 = "cat in the city"s;
+    const string content2 = "cat in the town"s;
+    const string content3 = "cat in the town or city"s;
+    const vector<int> ratings = { 1, 2, 3 };
+    SearchServer server;
+    server.AddDocument(doc_id1, content1, DocumentStatus::ACTUAL, ratings);
+    server.AddDocument(doc_id2, content2, DocumentStatus::REMOVED, ratings);
+    server.AddDocument(doc_id3, content3, DocumentStatus::REMOVED, ratings);
+    const auto found_docs = server.FindTopDocuments("in the cat"s, DocumentStatus::REMOVED);
+ 
+    {
+        ASSERT_HINT(found_docs[0].id == doc_id2, "Wrong status");
+        ASSERT_HINT(found_docs[1].id == doc_id3, "Wrong status");
+        ASSERT_HINT(found_docs.size() == 2, "Wrong status request");
+    }
+}
+void TestCalculateRelevance1() {
+    SearchServer server;
+    server.AddDocument(100, "white cat new ring"s, DocumentStatus::ACTUAL, { 1, 2, 3 });
     server.AddDocument(101, "fluffy cat fluffy tail"s, DocumentStatus::ACTUAL, { 1, 2, 3 });
     server.AddDocument(102, "good dog big eyes"s, DocumentStatus::ACTUAL, { 1, 2, 3 });
     const auto found_docs = server.FindTopDocuments("fluffy good cat"s);
-    double relevance_query = log((3 * 1.0) / 1) * (2.0 / 4.0) + log((3 * 1.0) / 2) * (1.0 / 4.0);
+    double relevance_query =  log((3 * 1.0) / 1) * (2.0 / 4.0) + log((3 * 1.0) / 2) * (1.0 / 4.0);
+  
+    {
+        ASSERT_HINT(fabs(found_docs[0].relevance - relevance_query) < 1e-6, "Wrong calculation relevance");
+       
+    }
+}
+ 
+void TestCalculateRelevance2() {
+    SearchServer server;
+    server.AddDocument(100, "cat in the city NY"s, DocumentStatus::ACTUAL, {255, -103, 4, 1});
+    server.AddDocument(101, "cat in the city Moscow"s, DocumentStatus::ACTUAL, {255, -103, 4, 1});
+    const auto found_docs = server.FindTopDocuments("cat NY"s);
+    double relevance_query =  log((2 * 1.0) / 2) * (1.0 / 3.0) + log((2 * 1.0) / 1) * (1.0 / 5.0);
+    
+    {
+        ASSERT_HINT(fabs(found_docs[0].relevance - relevance_query) < 1e-6, "Wrong calculation relevance");
+       
+    }
+
+}
+ 
+void TestCalculateRelevance3() {
+    SearchServer server;
+    server.AddDocument(100, "white cat new ring"s, DocumentStatus::ACTUAL, { 1, 2, 3 });
+    server.AddDocument(101, "fluffy cat fluffy tail"s, DocumentStatus::ACTUAL, { 1, 2, 3 });
+    server.AddDocument(102, "good dog big eyes"s, DocumentStatus::ACTUAL, { 1, 2, 3 });
+    const auto found_docs = server.FindTopDocuments("elden ring"s);
+    double relevance_query = log((3 * 1.0) / 1) * (1.0 / 4.0);
  
     {
         ASSERT_HINT(fabs(found_docs[0].relevance - relevance_query) < 1e-6, "Wrong calculation relevance");
@@ -182,9 +297,16 @@ void TestSearchServer() {
     RUN_TEST(TestMatchedDocuments);
     RUN_TEST(TestSortResultsByRelevance);
     RUN_TEST(TestCalculateDocumentRating);
+    RUN_TEST(TestCalculateDocumentMinusRating);
+    RUN_TEST(TestCalculateDocumentMixedRating);
     RUN_TEST(TestDocumentSearchByPredicate);
-    RUN_TEST(TestDocumentSearchByStatus);
-    RUN_TEST(TestCalculateRelevance);
+    RUN_TEST(TestDocumentSearchByStatusActual);
+    RUN_TEST(TestDocumentSearchByStatusIrrelevant);
+    RUN_TEST(TestDocumentSearchByStatusBanned);
+    RUN_TEST(TestDocumentSearchByStatusRemoved);
+    RUN_TEST(TestCalculateRelevance1);
+    RUN_TEST(TestCalculateRelevance2);
+    RUN_TEST(TestCalculateRelevance3);
 }
 
 
